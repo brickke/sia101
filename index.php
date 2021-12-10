7f91cf261d540e5e1ccc2bc8f97862833a6e5e38
brickke <2108806@gcci.edu.ph>
2021-12-10 13:54:54 +0800

    Edited PHP installation!

--- index.php
+++ index.php
@@ -5,72 +5,22 @@
 
 	<html>
 		<body>
-			<h1> DOCKER INSTALLATION</h1>
-			<p> In installing docker, either you choose in enabling WSL or directly download docker in windows</p>
-			<p>NOTE: Dont forget to check your windows features: WSL enabled, HyperV, and Virtual Machine</p>
-			
-			<p>In installing docker, go to docker.com</p>
-			<p>Get Started, copy command and paste it in your windows terminal. Make sure that your terminal is in Administrator command</p>
-			<p>Open another terminal and type wsl and youre good to go</p>
-			<p>
-					wsl -l -v checks the current version of youre running
-					wsl --set version ubuntu 20.04 2 (or any distro you have)
+			  <h1>PHP INSTALLATION</h1>
 
-					Go to your docker desktopif you have installed windows docker, go to settings, resources, wsl integration, check ubuntu and click apply and restore_include_path
+                <p>
+                        In installing the php using the command promp runas Administrator. Simply usethe co                        mmand install php7 orany php version available forthesystem.
 
-					sudo docker images ls-a to check images 
-					sudo su -root 
-					docker run hello-world (visit hubdocker.com forother repository)
+                        To check the php version installed in the computer, just type the command php versi                        on.
 
-					Make sure you have installed php and apache2
-
-					type yourecho php
-					
-					Now create a github repository
-
-					cd /var/www/html 
-					ls  to check lists
-					vim index php create and edit your index
-
-					sudo su root
-					touch index php create index
-					:wq in saving and exiting
-					mkdir in creating directory
-					mv -move index file to the specific directory you want
-					
-					git init - initialization, must be done first 
-					git status -check status of your git
-					git add -add index
-					git commit -m 
+                        Lastly, you can start coding after the installtion.
 
 
-					If encountered problem with showing the index in your github repository, and have trouble with token 
+                </p>
 
-						install gh by visiting gh-cli installation in your web browser
-						copy and paste the following command in the distro you have 
-						apt update -updates the whole terminal 
-						apt install gh -install gh 
-						gh auth login github
-						Select github, https, y
-						hit enter to Login with web browser
-						copy one time code in your web browser, lastly
-						git push --set-upstream origin master -push to your github repository
-						 
 
-						NOTE:  error like !bash event not found
-								type set +H 
-						
-				
-					
-						 
 
-			</p>
 
-			
-			
-		
-		</body>
-	</html>
-	
+                </body>
+        </html>		
 
 ?>
